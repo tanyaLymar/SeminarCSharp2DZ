03267002e295e1ed520593c3f20bbea8bdea09c0
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-22 00:05:07 +0300

    Task3 solved

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+bool isTrue = true;
+
+while(isTrue)
+{
+    Console.WriteLine("Введите номер координатной четверти");
+    int n = int.Parse(Console.ReadLine()?? "0");
+
+    switch(n)
+        {
+            case 1:
+                Console.WriteLine("Диапазон возможных значений: x (0; PositiveInfinity), y (0; PositiveInfinity");
+                isTrue = false;
+                break;
+
+            case 2:
+                Console.WriteLine("Диапазон возможных значений: x (0; NegativeInfinity), y (0; PositiveInfinity)");
+                isTrue = false;
+                break;
+
+            case 3:
+                Console.WriteLine("Диапазон возможных значений: x (0; NegativeInfinity), y (0; NegativeInfinity)");
+                 isTrue = false;
+                 break;
+
+            case 4:
+                Console.WriteLine("Диапазон возможных значений: x (0; PositiveInfinity), y (0; NegativeInfinity)");
+                 isTrue = false;
+                break;
+
+            default:
+                Console.WriteLine("Такой четверти не существует. Попробуйте еще раз.");
+                break;
+        }
+}
