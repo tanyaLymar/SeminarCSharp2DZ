819388fa300aa17b97c36631877ac9b4207cb80a
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-01-21 21:59:35 +0300

    Task1 solved

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if(number == 1|number ==2|number ==3| number ==4|number ==5) Console.WriteLine("Это не выходной день.");
+else if(number ==6| number == 7) Console.WriteLine("Это выходной день.");
+else Console.WriteLine("Попробуйте еще раз.");
